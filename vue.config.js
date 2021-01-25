21e57f9d037f7ebd5c892a89ae8d4dedaf58ce1b
ucntkhr <ucntkhr.gftdwks@gmail.com>
2021-01-25 15:14:21 +0900

    instant imvisible

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,2 @@
-module.exports = {
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.svg$/,
-          loader: 'vue-svg-loader', 
-        },
-      ],
-    }      
-  }
-};
+//hoge
 
-module.exports = {
-  chainWebpack: config => {
-    config.module.rules.delete("svg");
-  },
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.svg$/, 
-          loader: 'vue-svg-loader', 
-        },
-      ],
-    }      
-  }
-};
