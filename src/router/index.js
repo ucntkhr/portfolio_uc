416720a1d07111eefd6f716244b109573b4ee2e1
ucntkhr <ucntkhr.gftdwks@gmail.com>
2021-01-26 17:19:12 +0900

    Long farewell to About ( Delete About.vue / erase routing )

--- src/router/index.js
+++ src/router/index.js
@@ -14,29 +14,25 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
+
   {
     path: '/profile',
     name: 'Profile',
     component: Profile
   },
+
   {
     path: '/skillset',
     name: 'Skillset',
     component: Skillset
   },
+
   {
     path: '/project',
     name: 'Project',
     component: Project
   },
+
   {
     path: '/contact',
     name: 'Contact',
