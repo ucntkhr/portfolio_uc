7c776b404203d0488cdf74d3513c68ac966fa334
ucntkhr <ucntkhr.gftdwks@gmail.com>
2021-02-01 17:37:47 +0900

    Struggle hard : Make Project.vue / ProjectCard.vue / ProjectData.json / use Bulma Template "Cards" / but...

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,10 +1,53 @@
 <template>
 
 	<section id="project" class="section has-background-white-ter">
+
 		<div class="intro column is-8 is-offset-2">
+
 			<h2 class="title">PROJECTS</h2>
 			<p class="subtitle">Here are my <strong>WORKS</strong> , still <strong>"IN PROGRESS"</strong>.</p>
+
+			<ProjectCard v-for="ProjectData in ProjectDatas"  v-bind:key="ProjectData.Project_id" v-bind="ProjectData"/>
+
+
 		</div>
+
 	</section>
 
 </template>
+
+<script>
+import ProjectCard from '@/components/ProjectCard.vue';
+import ProjectData from '@/ProjectData.json';
+
+export default {
+
+	name: "Project",
+
+	components: {
+
+		ProjectCard,
+
+	},
+
+
+	data() {
+		return {
+			fontSize: '',
+			ProjectDatas: ProjectData
+
+
+		}
+		
+	},
+
+
+  created() {
+    if (this.icon !== 'fas fa-code') {
+      this.fontSize = 'font-size: x-large'
+    }
+  }
+
+}
+
+</script>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,123 @@
+<template>
+
+  <div class="container">
+    <div class="section">
+      <div id="app" class="row columns is-multiline">
+        <div class="column is-4">
+          <div class="card large">
+
+            <div class="card-image">
+
+              <figure class="image is-16by9">
+
+                <img src="@/assets/kouji.png">
+
+              </figure>
+
+            </div>
+
+            <div class="card-content">
+
+              <div class="media">
+
+                <div class="media-left">
+
+                  <figure class="image is-48x48">
+                    <img src="@/assets/logo.png">
+                  </figure>
+
+                </div>
+
+                <div class="media-content">
+                  <p class="title is-4 no-padding">{{ title }}</p>
+                </div>
+
+              </div>
+
+              <div class="content">
+
+                {{ desc }}
+
+              </div>
+
+              <div class="content">
+
+                <button class="button is-rounded is-primary">
+
+                  <span>{{ status }}</span>
+
+                </button>
+
+              </div>
+
+
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+  export default {
+    name: "ProjectCard",
+
+    props: {
+
+      title: String,
+
+      project_id: String,
+
+      desc: String,
+
+      status: String,
+
+    },
+
+
+  }
+
+</script>
+
+<style scoped>
+
+.card {
+  overflow: hidden;
+  background: ghostwhite;
+  color: var(--bg);
+}
+
+.card.large {
+  border-radius: 5px;
+}
+
+.title.no-padding {
+  margin-bottom: 0 !important;
+}
+
+.footer {
+  background: var(--card-bg);
+  color: ghostwhite;
+}
+
+.footer p,
+strong {
+  color: ghostwhite
+}
+
+.footer a:hover {
+  color: crimson;
+}
+
+.fa {
+  color: ghostwhite;
+  margin: 10px
+}
+
+
+</style>
